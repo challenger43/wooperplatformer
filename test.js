d2ab02e5240e4ef2d61a009cf79f06d7ea51e742
challenger43 <isabella.lo28@student.tamdistrict.org>
2026-02-28 11:50:23 -0800

    quag

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 import { trials } from './testCoords.js'
-const GRAVITY_DEFAULT = 350;
+const GRAVITY_DEFAULT = 380;
+const GRAVITY_QUAGSIRE = 500
 class testScene extends Phaser.Scene {
     player; //cannot use const for these because will need to change them later
     stars;
@@ -10,12 +11,14 @@ class testScene extends Phaser.Scene {
     cursors;
     score = 0;
     keys;
+    cursors; 
     constructor() {
         super({ key: 'testScene' });
     }
 
     preload() {
         this.load.image('ground', 'assets/platform.png');
+        this.load.spritesheet('quagsire', 'assets/quagsirespritesheet.png', { frameWidth: 32, frameHeight: 32 });
         this.load.spritesheet('dude', 'assets/wooperspritesheet1a.png', { frameWidth: 32, frameHeight: 32 });
         this.load.image('star', 'assets/WooperBall.png'); //they don't actually look like stars in 'real life' 
         this.load.image('bomb', 'assets/bomb.png');
@@ -28,13 +31,15 @@ class testScene extends Phaser.Scene {
         this.score += 10;
     }
     create() {
+        this.cursors = this.input.keyboard.createCursorKeys();
         this.keys = this.input.keyboard.addKeys('W,A,S,D,Q,P,O,V,SPACE');
-        this.anims.create({
+                this.anims.create({
             key: 'left',
             frames: this.anims.generateFrameNumbers('dude', { start: 1, end: 4 }),
             frameRate: 20,
             repeat: -1
         });
+
         this.anims.create({
             key: 'turn',
             frames: [{ key: 'dude', frame: 0 }],
@@ -47,6 +52,23 @@ class testScene extends Phaser.Scene {
             frameRate: 20,
             repeat: -1
         });
+        this.anims.create({
+            key: 'quagLeft',
+            frames: this.anims.generateFrameNumbers('quagsire', { start: 3, end: 4 }),
+            frameRate: 4,
+            repeat: -1
+        })
+        this.anims.create({
+            key: 'quagRight',
+            frames: this.anims.generateFrameNumbers('quagsire', { start: 1, end: 2 }),
+            frameRate: 4,
+            repeat: -1
+        })
+        this.anims.create({
+            key: 'quagStill',
+            frames: [{ key: 'quagsire', frame: 0, }],
+            frameRate: 20,
+        })
         this.stars = this.physics.add.group();
         for (let starData of trials.stars) {
             this.stars.create(starData.x, starData.y, 'star')
@@ -83,20 +105,65 @@ class testScene extends Phaser.Scene {
     }
 
     update() {
-        if (this.keys.A.isDown){
-            this.player.setVelocityX(-80)
-            this.player.anims.play('left', true)
+        if (this.quagsire) {
+            this.physics.world.gravity.y = GRAVITY_QUAGSIRE;
         }
-        else if (this.keys.D.isDown){
-            this.player.setVelocityX(80)
-            this.player.anims.play('right', true);
+        else if (this.isInWater) {
+            this.physics.world.gravity.y = GRAVITY_WATER; //when inside water, gravity is set to 250
+            // this.player.setGravityY(GRAVITY_WATER);
         }
-        else{
+        else {
+            this.physics.world.gravity.y = GRAVITY_DEFAULT;
+            // this.player.setGravityY(GRAVITY_DEFAULT);
+        }
+        if (this.keys.Q.isDown) {
+            this.stars.children.iterate((star) => this.collectStar(this.player, star));
+            this.floatingStars.children.iterate((star) => this.collectFloatingStar(this.player, star));
+        } // put this back in after presentation
+        if (this.keys.A.isDown || this.cursors.left.isDown) {
+            if (this.quagsire == true) {
+                this.player.setVelocityX(this.isInWater ? -130 : -80);
+                // console.log("Trying to play quagsire_left");
+                this.player.anims.play('quagLeft', true);
+            }
+            else {
+                this.player.anims.play('left', true);
+                this.player.setVelocityX(this.isInWater ? -100 : -160);
+            }
+        }
+        else if (this.keys.D.isDown || this.cursors.right.isDown) {
+            if (this.quagsire == true) {
+                this.player.setVelocityX(this.isInWater ? 130 : 80);
+                // console.log("Trying to play quagsire_right");
+                this.player.anims.play('quagRight', true)
+            }
+            else {
+                this.player.anims.play('right', true);
+                this.player.setVelocityX(this.isInWater ? 100 : 160);
+            }
+        }
+        else { //if no key is pressed, will face forwards
             this.player.setVelocityX(0)
-            this.player.anims.play('turn', true)
+            if (this.quagsire == true) {
+                this.player.anims.play('quagStill', true)
+            }
+            else {
+                this.player.anims.play('turn');
+            }
         }
-        if ((this.keys.W.isDown || this.keys.SPACE.isDown) && this.player.body.touching.down){
-            this.player.setVelocityY(-200);
+        if ((this.keys.W.isDown || this.keys.SPACE.isDown || this.cursors.up.isDown) && ((this.isInWater || this.player.body.touching.down))) { 
+            this.player.setVelocityY(this.isInWater ? -100 : -280);
+        }
+        if (!["LevelOne", "LevelTwo"].includes(this.scene.key)) {
+            if (this.keys.P.isDown && !this.wasPDownLastFrame) {
+                this.quagsire = !this.quagsire;
+                if (this.quagsire) {
+                    this.player.setTexture('quagsire');
+                } else {
+                    this.player.setTexture('dude')
+                }
+            }
+            this.wasPDownLastFrame = this.keys.P.isDown;
         }
         this.movingPlatforms.children.iterate(movingPlatform => {
             if (movingPlatform.movementType === 'pingpong') {
